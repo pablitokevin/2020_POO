4c18eabd50ef5c7a9222f054f932139c9341cd27
Pablo Kevin Marinho da Silva <p.kevin@academico.ifrn.edu.br>
2020-02-21 15:55:28 -0300

    Testando

---
+++ Lista01/Exercicio 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Bem vindo, {s}");
+            Console.ReadKey();
+        }
+    }
+}
